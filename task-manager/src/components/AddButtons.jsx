79c9cab7f5606f4e5c749eb1c6984b2c4676daf2
ZenzeroBro <165959392+ZenzeroBro@users.noreply.github.com>
2024-07-29 16:36:39 -0700

    Collin Boyer monday AddButtons work
    here's the addButtons file updated to make it work like we want

--- task-manager/src/components/AddButtons.jsx
+++ task-manager/src/components/AddButtons.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState }from "react";
 
-function Add_button() {
+function Add_button(props) {
+  const [note, make_note] = useState({
+    title: "",
+    content: "",
+  });
+
+  function doChange(event) {
+    const { name, value } = event.target;
+
+    make_note((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submit_note(event) {
+    props.onAdd(note);
+    make_note({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
     return (
       <div className="submit_boxes">
-        <button type="submit">Add New List button</button>
-        <button type="text">List Name</button>
-        <button >List Due date</button>
+        <button type="submit" onClick={submit_note}>Add New List button</button>
+        <input
+          name="title"
+          onChange={doChange}
+          value={note.title}
+          placeholder="List Name "
+        />
+        <textarea
+          name="content"
+          onChange={doChange}
+          value={note.content}
+          placeholder="List Due Date"
+          rows="3"
+        />
         <button >Overview</button>
       </div>
     );
